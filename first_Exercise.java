dd628c86b3ac16d057dba65f72c31ed0119fdc30
Michael Charles Nolan <48270695+miketheocwguy@users.noreply.github.com>
2023-07-12 22:59:36 -0400

    Create first_Exercise.java

---
+++ first_Exercise.java
@@ -0,0 +1,26 @@
+package first_Exercise;
+import java.util.Scanner;
+
+public class myfirst {
+
+	public static void main(String[] args) {
+	 
+	int numLocal , numInternational;
+	double percentLocal, percentInternational, Total;
+	 
+		    Scanner sc = new Scanner(System.in);
+	      System.out.println("How many local students");
+	      numLocal = sc.nextInt();
+	      System.out.println("How many internationals students");
+	      numInternational = sc.nextInt();
+	      
+	      Total = numLocal + numInternational;
+	      percentLocal = numLocal/(Total*100);
+	      percentInternational = numInternational/(Total*100);
+	      
+	      System.out.print("Total number of students is " + Total + " of which " + percentLocal+"% are local and "+percentInternational+"% are international" );
+	      
+	      
+	}
+
+}
