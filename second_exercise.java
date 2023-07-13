589b61f3b7d26694f0adef45421b10302b2bf189
Michael Charles Nolan <48270695+miketheocwguy@users.noreply.github.com>
2023-07-12 23:51:21 -0400

    Create second_exercise.java

---
+++ second_exercise.java
@@ -0,0 +1,42 @@
+package second_exercise;
+import java.util.Scanner;
+
+public class secound_exercise {
+
+	public static void main(String[] args) {
+     
+	Scanner ans = new Scanner(System.in);
+	double income;
+	double tax = 0;
+	
+	System.out.println("Input your income: ");
+	income = ans.nextDouble();
+	
+	
+	if(income <= 48000) {
+		tax = income*(0.15);
+		
+	}
+	else if((income > 48000)&&(income < 97000)) {
+		tax = income*(0.205);
+		
+	}
+	else if((income > 97000)&&(income < 150000)) {
+		tax = income*(0.26);
+			
+	}
+	else if((income > 150000)&&(income < 214000)) {
+		tax = income*(0.29);
+			
+	}
+	else if (income > 214000) {
+		tax = income*(0.33);
+	}
+	
+	ans.close();
+	System.out.println("Tax amount is: " +tax);
+
+    
+
+	}
+}
